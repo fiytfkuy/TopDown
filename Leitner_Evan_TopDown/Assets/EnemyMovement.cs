ad13a573654bf73f1d0f64aae8a6cd7fc0519f9e
Evan Leitner <evandan789@gmail.com>
2021-04-28 17:03:06 -0700

    Initial Project Upload

---
+++ Leitner_Evan_TopDown/Assets/EnemyMovement.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float detectionRadius = 3;
+    public float movementSpeed = 5;
+    public bool canMove = false;
+    public bool movementDirection = false; //false goes down, true goes up
+    public bool isFollowing = false;
+
+    public Transform playerTarget;
+    private Animator myAnimator; 
+    private Rigidbody2D myRB;
+    private CircleCollider2D detectionZone;
+    private Vector2 up;
+    private Vector2 down;
+    private Vector2 zero;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        up = new Vector2(0, movementSpeed);
+        down = new Vector2(0, -movementSpeed);
+        zero = new Vector2(0, 0);
+
+        playerTarget = GameObject.Find("PlayerSprite").transform;
+        
+        myRB = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        detectionZone = GetComponent<CircleCollider2D>();
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        detectionZone.radius = detectionRadius;
+
+        if (isFollowing == false)
+        {
+            myAnimator.SetBool("IsWalking", false);
+            myRB.velocity = zero;
+        }
+
+        else if (isFollowing == true)
+        {
+            Vector3 lookPos = playerTarget.position - transform.position;
+            float angle = Mathf.Atan2(lookPos.y, lookPos.x) * Mathf.Rad2Deg;
+            myRB.rotation = angle;
+            lookPos.Normalize();
+
+            myAnimator.SetBool("IsWalking", true);
+
+            myRB.MovePosition(transform.position + (lookPos * movementSpeed * Time.deltaTime));
+
+        }
+
+        if (canMove == true)
+        {
+            if (movementDirection == true)
+                myRB.velocity = up;
+            else if (movementDirection == false)
+                myRB.velocity = down;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name.Contains("Bullet"))
+        {
+            Destroy(collision.gameObject);
+            GameObject.Find("GameManager").GetComponent<GameManager>().playerKillCount++;
+            this.gameObject.SetActive(false);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name.Contains("Player"))
+            isFollowing = true;
+
+        if (collision.gameObject.name == "Trigger1")
+            movementDirection = false;
+
+        else if (collision.gameObject.name == "Trigger2")
+            movementDirection = true;
+
+    }
+
+    private void OntriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.name.Contains("Player"))
+            isFollowing = false;
+
+    }
+
+
+
+
+
+
+}
+
+    
+    
+ 
---
+++ Leitner_Evan_TopDown/Assets/PlayerController.cs
@@ -0,0 +1,171 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D myRB;
+    private AudioSource speaker;
+    public float speed = 10;
+    public float bulletLifespan = 1;
+    public int playerHealth = 3;
+    public GameObject bullet;
+    public float bulletspeed = 15;
+    public AudioClip shootSoundEffect;
+    public AudioClip punchSoundEffect;
+    public AudioClip pickupSoundEffect;
+
+    private bool canShoot = true;
+    public float shootCooldownTime;
+    private float timeDifference;
+    public bool invincible = false;
+    public float invincibleCooldownTime;
+    private float timeDifference2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRB = GetComponent<Rigidbody2D>();
+        speaker = GetComponent<AudioSource>();
+        playerHealth = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerHealth <= 0)
+        {
+            transform.SetPositionAndRotation(new Vector2(), new Quaternion());
+            playerHealth = 3;
+        }
+
+        Vector2 velocity = myRB.velocity;
+
+        velocity.x = Input.GetAxisRaw("Horizontal") * speed;
+        velocity.y = Input.GetAxisRaw("Vertical") * speed;
+
+        myRB.velocity = velocity;
+
+        if (canShoot)
+        {
+
+
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                GameObject b = Instantiate(bullet, new Vector2(transform.position.x, transform.position.y + 1), transform.rotation);
+                b.GetComponent<Rigidbody2D>().velocity = new Vector2(0, bulletspeed);
+
+                speaker.clip = shootSoundEffect;
+                speaker.Play();
+
+                Destroy(b, bulletLifespan);
+
+                canShoot = false;
+            }
+
+            else if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                GameObject b = Instantiate(bullet, new Vector2(transform.position.x, transform.position.y - 1), transform.rotation);
+                b.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -bulletspeed);
+
+                speaker.clip = shootSoundEffect;
+                speaker.Play();
+
+                Destroy(b, bulletLifespan);
+
+                canShoot = false;
+            }
+
+            else if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                GameObject b = Instantiate(bullet, new Vector2(transform.position.x - 1, transform.position.y), transform.rotation);
+                b.GetComponent<Rigidbody2D>().velocity = new Vector2(-bulletspeed, 0);
+
+                speaker.clip = shootSoundEffect;
+                speaker.Play();
+
+                Debug.Log(b.GetComponent<Rigidbody2D>().velocity);
+
+                Destroy(b, bulletLifespan);
+
+                canShoot = false;
+            }
+
+            else if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                GameObject b = Instantiate(bullet, new Vector2(transform.position.x + 1, transform.position.y), transform.rotation);
+                b.GetComponent<Rigidbody2D>().velocity = new Vector2(bulletspeed, 0);
+
+                speaker.clip = shootSoundEffect;
+                speaker.Play();
+
+                Destroy(b, bulletLifespan);
+
+                canShoot = false;
+            }
+        }
+
+        if(canShoot == false)
+        {
+            timeDifference += Time.deltaTime;
+
+            Debug.Log(timeDifference);
+
+            if (timeDifference >= shootCooldownTime)
+            {
+                canShoot = true;
+                timeDifference = 0;
+
+            }
+
+        }
+
+        if (invincible == true)
+        {
+            timeDifference2 += Time.deltaTime;
+
+            Debug.Log(timeDifference2);
+
+            if (timeDifference >= invincibleCooldownTime)
+            {
+                invincible = false;
+                timeDifference2 = 0;
+            }
+        }
+    }
+   
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "EnemySprite")
+        {
+            //This also means playerHealth = playerHealth - 1;
+
+            speaker.clip = punchSoundEffect;
+            speaker.Play();
+
+            playerHealth--;
+        }
+
+        else if ((collision.gameObject.name == "Pickup") && (playerHealth < 3))
+        {
+
+            playerHealth++;
+
+            speaker.clip = pickupSoundEffect;
+            speaker.Play();
+
+            collision.gameObject.SetActive(false);
+        }
+    }
+        
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.name.Contains("enemy"))
+        {
+            GameObject.Find("EnemySprite").GetComponent<EnemyMovement>().canMove = true;
+            GameObject.Find("EnemySprite").GetComponent<Rigidbody2D>().constraints &= ~RigidbodyConstraints2D.FreezePositionY;
+            Destroy(collision.gameObject);
+        }
+    }
+}
